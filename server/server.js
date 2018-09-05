8e090c0b568cef79b0fdec8906ea41cc645a912d
unknown <arifluthfi16@gmail.com>
2018-09-05 22:51:56 +0700

    Added get todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//Bakal Ngelakuin sesuatu buat post request
 app.post("/todos", (req,res)=>{
 
     var todo = new Todo({
@@ -23,6 +24,17 @@ app.post("/todos", (req,res)=>{
         res.status(400).send(e);
     });
 })
+
+//Bakal Ngelakuin sesuatu buat get request
+app.get("/todos", (req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({
+            todos
+        })
+    },(e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, ()=>{
     console.log("Started on port 3000");
